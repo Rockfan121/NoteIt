e6a1498d2bb044399c9fd1ad53bdcc46f17ec6b5
Marekp95 <marek-313-95@o2.pl>
2017-05-20 13:53:59 +0200

    google login button

--- Frontend/src/routes/Home/components/HomeView.js
+++ Frontend/src/routes/Home/components/HomeView.js
@@ -1,17 +1,36 @@
 import React from 'react'
-import DuckImage from '../assets/Duck.jpg'
 import { Link } from 'react-router'
 import './HomeView.scss'
+import GoogleLogin from 'react-google-login'
 
-export const HomeView = () => (
-  <div className='homeContent'>
-    <h4>Welcome!</h4>
-    <Link to='/notes'>
-      <h1>Notes</h1>
-    </Link>
-    <img
-      src={DuckImage} />
-  </div>
-)
+
+// eslint-disable-next-line react/prefer-es6-class
+class HomeView extends React.Component {
+  responseGoogle = (response) => {
+
+  }
+
+  render () {
+    return (
+      <div className='homeContent'>
+        <h4>Welcome!</h4>
+        <GoogleLogin
+          clientId='571224980832-uanbm1kfqji3da8dq326v2b08vfv7akq.apps.googleusercontent.com'
+          buttonText='Login'
+          onSuccess={
+            (e) => this.responseGoogle(e)
+          }
+          onFailure={
+            (e) => this.responseGoogle(e)
+          }
+        >G+ login</GoogleLogin>
+        <Link to='/notes'>
+          <h1>Notes</h1>
+        </Link>
+
+      </div>
+    )
+  }
+}
 
 export default HomeView
