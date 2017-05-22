1bd9b1a98b03144ffc2585384114f0b46ccb0827
Marekp95 <marek-313-95@o2.pl>
2017-05-22 23:17:39 +0200

    imports

--- backend/src/main/java/pl/edu/agh/notelt/model/User.java
+++ backend/src/main/java/pl/edu/agh/notelt/model/User.java
@@ -2,7 +2,14 @@ package pl.edu.agh.notelt.model;
 
 import org.assertj.core.util.Lists;
 
-import javax.persistence.*;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.OneToMany;
 import java.util.List;
 
 @Entity(name = "user")
--- backend/src/main/java/pl/edu/agh/notelt/rest/NoteRest.java
+++ backend/src/main/java/pl/edu/agh/notelt/rest/NoteRest.java
@@ -1,7 +1,11 @@
 package pl.edu.agh.notelt.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import pl.edu.agh.notelt.model.Note;
 import pl.edu.agh.notelt.service.NoteService;
 
