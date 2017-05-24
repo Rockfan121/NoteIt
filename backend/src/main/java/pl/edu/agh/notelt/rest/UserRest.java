962c8a2ce78d0677ed9bdcf70a8bc4a01288f34d
Marekp95 <marek-313-95@o2.pl>
2017-05-24 08:04:16 +0200

    bugfix

--- backend/src/main/java/pl/edu/agh/notelt/rest/UserRest.java
+++ backend/src/main/java/pl/edu/agh/notelt/rest/UserRest.java
@@ -5,13 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import pl.edu.agh.notelt.model.Note;
 import pl.edu.agh.notelt.model.User;
 import pl.edu.agh.notelt.service.NoteService;
@@ -91,7 +85,7 @@ public class UserRest {
                         new ObjectMapper().readValue(response.toString(), HashMap.class);
                 //CHECKSTYLE:ON
                 final String email = result.get("email").toString();
-                if (userService.getUserByName(email) == null) {
+                if (!userService.getUserByName(email).isPresent()) {
                     userService.saveUser(new User(email));
                 }
                 final User user = userService.getUserByName(email).get();
