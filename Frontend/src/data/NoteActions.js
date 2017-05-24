f52cb66a54bbe3befcbd5f3eb2b4fa32968b1b40
rockzzz <k.mura121@gmail.com>
2017-05-24 06:06:08 +0200

    Implement actions for notes

--- Frontend/src/data/NoteActions.js
+++ Frontend/src/data/NoteActions.js
@@ -13,3 +13,66 @@ export const fetchNotes = (id) => {
       })
   }
 }
+
+export const createNote = (data) => {
+  return(dispatch) => { 
+    const userId = data.userId
+    const url = 'api/users/{userId}/notes'
+
+    const title = data.title
+    const content = data.content
+    return api
+      .post(url, {
+        title,
+        content,
+      })
+      .then((response) => {
+        console.log("New note has been successfully created")
+      })
+      .catch((error) => {
+        console.error("Error: "+error)
+      })
+  }
+}
+
+export const updateNote = (data) => {
+  return(dispatch) => { 
+    const userId = data.userId
+    const id= data.id
+    const url = '/notes/{id}'
+
+    const title = data.title
+    const content = data.content
+    return api
+      .put(url, {
+        title,
+        content,
+        userId,
+      })
+      .then((response) => {
+        console.log("Note has been successfully updated")
+      })
+      .catch((error) => {
+        console.error("Error: "+error)
+      })
+  }
+}
+
+export const deleteNote = (data) => {
+  return(dispatch) => { 
+    const userId = data.userId
+    const id = data.id
+    const url = '/notes/{id}'
+
+    return api
+      .put(url, {
+        userId,
+      })
+      .then((response) => {
+        console.log("Note has been successfully updated")
+      })
+      .catch((error) => {
+        console.error("Error: "+error)
+      })
+  }
+}
